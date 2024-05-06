12cb0af072e93077d7f6ebd1aab9de9cac7abc3d
Hani <hhoumimid@gmail.com>
2024-05-06 15:11:09 +0200

    Changement pour coder en Vue.js, création du dossier de projet et implémentation de l'échiquier en Vue.

---
+++ front/echiquier/src/components/echiquier.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="board">
+      <div class="row" v-for="row in rows" :key="row.id">
+        <div class="square" v-for="square in row.squares" :key="square.id" :class="[square.color]">
+        {{ square.label }}
+        <img v-if="square.image" :src="square.image" alt="Pawn">
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        rows: [
+          { id: 1, squares: [{ id: 1, label: '1', color: 'light' }, { id: 2, color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 2, squares: [{ id: 1, label: '2', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
+          { id: 3, squares: [{ id: 1, label: '3', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 4, squares: [{ id: 1, label: '4', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
+          { id: 5, squares: [{ id: 1, label: '5', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 6, squares: [{ id: 1, label: '6', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
+          { id: 7, squares: [{ id: 1, label: '7', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 8, squares: [{ id: 1, label: '8    h', color: 'dark' }, { id: 2, label:'g', color: 'light' }, { id: 3, label:'f', color: 'dark' }, { id: 4, label:'e', color: 'light' }, { id: 5, label:'d', color: 'dark' }, { id: 6, label:'c', color: 'light' }, { id: 7, label:'b', color: 'dark' }, { id: 8, label:'a', color: 'light' }] }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .light {
+      background-color: #f0d9b5; /* Couleur pour les cases claires */
+  }
+  
+  .dark {
+      background-color: #b58863; /* Couleur pour les cases foncées */
+  }
+  
+  #board {
+      display: flex;
+      flex-wrap: wrap;
+      width: 400px;
+  }
+  
+  .row {
+      display: flex;
+      width: 100%;
+  }
+  
+  .square {
+      width: 50px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid #000; /* Ajoute une bordure pour distinguer les cases */
+      color: #000; /* Couleur du texte dans les cases */
+  }
+  </style>
+  
--- front/fichier.js
+++ front/fichier.js
@@ -10,10 +10,31 @@ if (div2) {
         var child = div2Children[i];
 
         var imgPB = document.createElement("img");
-        imgPB.src = "../img/black_pawn.svg";
+        imgPB.src = "echiquier/public/image/black_pawn.svg";
 
     
         child.appendChild(imgPB);
+    }
+} else {
+    console.error("Aucun élément avec l'ID '2' trouvé.");
+}
+
+
+var div7 = document.getElementById('7');
+
+
+if (div7) {
+    // Sélection de tous les enfants de l'élément avec l'ID "7"
+    var div7Children = div7.children;
+
+    for (var i = 0; i < div7Children.length; i++) {
+        var childs = div7Children[i];
+
+        var imgPBs = document.createElement("img");
+        imgPBs.src = "img/white_pawn.svg";
+
+    
+        childs.appendChild(imgPBs);
     }
 } else {
     console.error("Aucun élément avec l'ID '2' trouvé.");
