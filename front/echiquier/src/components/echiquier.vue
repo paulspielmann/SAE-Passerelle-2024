e01769f7c6a1c00364d00e906c49b637e08627f3
Hani <hhoumimid@gmail.com>
2024-05-06 15:27:17 +0200

    Suppressions de fichiers inutiles. Changement des label pour avoir le bon ordre sur l'échiquier.

--- front/echiquier/src/components/echiquier.vue
+++ front/echiquier/src/components/echiquier.vue
@@ -14,14 +14,14 @@
     data() {
       return {
         rows: [
-          { id: 1, squares: [{ id: 1, label: '1', color: 'light' }, { id: 2, color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
-          { id: 2, squares: [{ id: 1, label: '2', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
-          { id: 3, squares: [{ id: 1, label: '3', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
-          { id: 4, squares: [{ id: 1, label: '4', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
-          { id: 5, squares: [{ id: 1, label: '5', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
-          { id: 6, squares: [{ id: 1, label: '6', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
-          { id: 7, squares: [{ id: 1, label: '7', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
-          { id: 8, squares: [{ id: 1, label: '8    h', color: 'dark' }, { id: 2, label:'g', color: 'light' }, { id: 3, label:'f', color: 'dark' }, { id: 4, label:'e', color: 'light' }, { id: 5, label:'d', color: 'dark' }, { id: 6, label:'c', color: 'light' }, { id: 7, label:'b', color: 'dark' }, { id: 8, label:'a', color: 'light' }] }
+          { id: 1, squares: [{ id: 1, label: '8', color: 'light' }, { id: 2, color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 2, squares: [{ id: 1, label: '7', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
+          { id: 3, squares: [{ id: 1, label: '6', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 4, squares: [{ id: 1, label: '5', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
+          { id: 5, squares: [{ id: 1, label: '4', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 6, squares: [{ id: 1, label: '3', color: 'dark' }, { id: 2, color: 'light' }, { id: 3, color: 'dark' }, { id: 4, color: 'light' }, { id: 5, color: 'dark' }, { id: 6, color: 'light' }, { id: 7, color: 'dark' }, { id: 8, color: 'light' }] },
+          { id: 7, squares: [{ id: 1, label: '2', color: 'light' }, { id: 2,  color: 'dark' }, { id: 3, color: 'light' }, { id: 4, color: 'dark' }, { id: 5, color: 'light' }, { id: 6, color: 'dark' }, { id: 7, color: 'light' }, { id: 8, color: 'dark' }] },
+          { id: 8, squares: [{ id: 1, label: '1   a', color: 'dark' }, { id: 2, label:'b', color: 'light' }, { id: 3, label:'c', color: 'dark' }, { id: 4, label:'d', color: 'light' }, { id: 5, label:'e', color: 'dark' }, { id: 6, label:'f', color: 'light' }, { id: 7, label:'g', color: 'dark' }, { id: 8, label:'h', color: 'light' }] }
         ]
       };
     }
